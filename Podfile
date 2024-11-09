d3a0f17a1d8035e1037940c0481233fa0b746b0f
Tahiatul Islam <tahiatul@tahiatuls-mbp.myfiosgateway.com>
2024-11-08 20:45:29 -0500

    init the socket app

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'socketNewtork' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Socket.IO-Client-Swift', '~> 16.1.1'
+
+end
---
+++ socketNewtork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  socketNewtork
+//
+//  Created by Tahiatul Islam on 11/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
